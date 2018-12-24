fdc904fa472822ad39032f38223a9d00fe79c948
wyu <kharma@gmail.com>
2018-12-23 20:18:43 -0800

    organize repo

--- trainspotting.sh
+++
@@ -1,6 +0,0 @@
-# Bash script to deploy model in Google Colaboratory
-# !git clone https://github.com/walteryu/trashspotting
-# !cd ./trashspotting/tensorflow_toy_detector/
-# !pwd
-# !ls -al ./
-!python ./trashspotting/tensorflow_toy_detector/create_pet_tf_record.py
