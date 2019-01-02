536c147bb2b63f672ae7f5b34bb4da028d5e82eb
wyu <kharma@gmail.com>
2019-01-02 08:17:23 -0800

    update import statement

--- 08_racoon_dataset.sh
+++ 08_racoon_dataset.sh
@@ -15,6 +15,9 @@ import os
 os.environ['PYTHONPATH'] += ':/content/models/research/:/content/models/research/slim/'
 !python object_detection/builders/model_builder_test.py
 
+# This is needed to display the images.
+# %matplotlib inline
+
 # Import modules (TF_ObjectDetection_API):
 import numpy as np
 import os
@@ -56,15 +59,37 @@ from object_detection.utils import ops as utils_ops
 # if tf.__version__ < '1.4.0':
 #   raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
 
-# This is needed to display the images.
-%matplotlib inline
-
 # Here are the imports from the object detection module.
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 
 # Set paths
 PATH_TO_API = '/content/models/research/object_detection'
-PATH_TO_REPO = '/content/models/research/object_detection/TF_ObjectDetection_API'
-PATH_TO_DATA = '/content/models/research/object_detection/TF_ObjectDetection_API/data'
-PATH_TO_LABELS = '/content/models/research/object_detection/TF_ObjectDetection_API/labels'
+PATH_TO_REPO = '/content/models/research/object_detection/raccoon_dataset'
+PATH_TO_DATA = '/content/models/research/object_detection/raccoon_dataset/data'
+PATH_TO_LABELS = '/content/models/research/object_detection/raccoon_dataset/annotations'
+
+!echo 'git clone TF_ObjectDetection_API'
+!echo '===> cd $PATH_TO_API'
+%cd $PATH_TO_API
+!echo ''
+!echo '===> ls $PATH_TO_API'
+!ls -al $PATH_TO_API
+!echo ''
+!rm -rf ./TF_ObjectDetection_API
+!git clone https://github.com/walteryu/raccoon_dataset
+!echo ''
+!echo '===> cd $PATH_TO_REPO'
+%cd $PATH_TO_REPO
+!echo ''
+!echo '===> ls $PATH_TO_REPO'
+!ls -al $PATH_TO_REPO
+!echo ''
+
+# Create TF record
+!echo '===> xml_to_csv.py'
+!python xml_to_csv.py
+!echo ''
+!echo '===> generate_tfrecord.py'
+!python generate_tfrecord.py
+!echo ''
